57b47345654ffe497eac7e8afa5bd0bcc22ffe62
SockBoxCrab <79289013+SockBoxCrab@users.noreply.github.com>
2022-01-30 15:55:22 -0700

    Uploaded Project

---
+++ Stealth Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerForm;
+
+    private float yOffset = 4.25f;//Used to keep camera certain distance away
+    private float zOffset = -7.5f;//from the player as they move
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        float posX = playerForm.position.x;
+        float posY = playerForm.position.y + yOffset;
+        float posZ = playerForm.position.z + zOffset;
+        Vector3 pos = new Vector3(posX, posY, posZ);
+        gameObject.transform.position = pos;
+    }
+}
